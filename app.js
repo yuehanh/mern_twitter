05842e106d490a798534ed253ed69016dd5ff9e4
Yuehan <yuehan.huang@protonmail.com>
2020-10-25 19:44:34 -0700

    Create mern-twitter

--- app.js
+++ app.js
@@ -3,10 +3,14 @@ const app = express();
 const mongoose = require("mongoose");
 const db = require("./config/keys").mongoURI;
 
-app.get("/", (req, res) => res.send("Hello World!!"));
-
 // We will later be deploying our app to Heroku, which requires us to run our server on process.env.PORT
 const port = process.env.PORT || 5000;
+
+const users = require("./routes/api/users");
+const tweets = require("./routes/api/tweets");
+const bodyParser = require("body-parser");
+
+const passport = require("passport");
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
 
@@ -14,3 +18,12 @@ mongoose
   .connect(db, { useNewUrlParser: true })
   .then(() => console.log("Connected to MongoDB successfully"))
   .catch((err) => console.log(err));
+
+app.use(passport.initialize());
+require("./config/passport")(passport);
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use("/api/users", users);
+app.use("/api/tweets", tweets);
---
+++ frontend/src/reducers/root_reducer.js
@@ -0,0 +1,13 @@
+// src/reducers/root_reducer.js
+
+import { combineReducers } from "redux";
+import session from "./session_api_reducer";
+import errors from "./errors_reducer";
+import tweets from "./tweets_reducer";
+const RootReducer = combineReducers({
+  session,
+  errors,
+  tweets,
+});
+
+export default RootReducer;
